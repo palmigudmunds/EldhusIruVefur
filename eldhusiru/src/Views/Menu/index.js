92f394d52c91a57fc86215261680d933fb9a2644
palmigudmunds <palmig20@ru.is>
2023-01-06 13:19:04 +0000

    Change padding for mobile and image size

--- eldhusiru/src/Views/Menu/index.js
+++ eldhusiru/src/Views/Menu/index.js
@@ -17,7 +17,7 @@ const FrontPageView = () => {
 			<Suspense fallback={<Loading/>}>
 				<div className='relative md:px-6 px-1 mb-32'>
 					<Navigation />
-					<div className='px-6 md:px-0 pt-32 md:pt-48 space-y-24'>
+					<div className='px-3 md:px-0 pt-32 md:pt-48 space-y-24'>
 						<FoodMenuHeader />
 						<FoodMenuItems />
 					</div>
