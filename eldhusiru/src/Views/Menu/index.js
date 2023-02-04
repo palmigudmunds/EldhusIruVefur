d5102915ec09ea50df049fedd5b548f6eaa3fbbd
palmigudmunds <palmig20@ru.is>
2023-02-04 13:48:03 +0000

    Added padding to menu on mobile and made nav logo slightly bigger again

--- eldhusiru/src/Views/Menu/index.js
+++ eldhusiru/src/Views/Menu/index.js
@@ -17,7 +17,7 @@ const MenuView = () => {
 			<Suspense fallback={<Loading/>}>
 				<div className='relative md:px-6 mb-32'>
 					<Navigation />
-					<div className='md:px-0 pt-9 md:pt-48 space-y-16 md:space-y-24'>
+					<div className='md:px-0 px-3 pt-9 md:pt-48 space-y-16 md:space-y-24'>
 						<FoodMenuHeader />
 						<FoodMenuItems />
 					</div>
