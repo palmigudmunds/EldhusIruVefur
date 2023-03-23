2927d7661eab7c685b3f2f667c942ded4638d0fc
palmigudmunds <palmig20@ru.is>
2023-03-23 15:23:29 +0000

    Hero button links to yess.is

--- eldhusiru/src/components/MainMenu/index.js
+++ eldhusiru/src/components/MainMenu/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import diskur from '../../resources/stadurinn.jpeg';
 
 
 const MainMenu = () => {
 
-    let navigate = useNavigate();
-
     const { t } = useTranslation();
     
     return (
         <div id="main-section" className="flex flex-col md:flex-row justify-start items-center max-w-none md:max-w-screen-xl mx-auto md:max-h-screen space-x-0 md:space-x-4">
-            <div className="w-full md:w-1/2 md:pt-16">
+            <div className="w-full md:w-1/2">
                 <h1 className="max-md:mt-5">{t('header-main')}</h1>
-                <h3 className="pt-5">{t('header-phone-text')}</h3>
-                <div className="pt-5">
-                    <button
+                <div className="pt-10">
+                    <a
+                        href='https://yess.is/eldhus-iru'
                         className="bg-main text-white text-lg md:text-xl font-medium py-4 px-8 rounded-full hover:bg-red-700 ease-in-out"
-                        onClick={()=> navigate('/menu')}>{t('see-menu-button')}</button>
+                        >{t('order-now-button')}</a>
                 </div>
             </div>
             <div className="flex justify-center w-full h-full md:w-1/2 md:max-h-screen mt-5 md:mt-0">
